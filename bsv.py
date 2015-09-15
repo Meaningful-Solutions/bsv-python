1b736c81b921bbea9af4d010f22304435c523908
ChopperCodes <website@meaningful-solutions.com>
2015-09-15 17:41:17 -0500

    Lots of fixes
    Updated version went through slightly more demanding testing than before
    and as a result is better able to align with the requirements.

--- bsv.py
+++ bsv.py
@@ -1,59 +1,74 @@
 from collections import deque
 
-def readBsv(fileName, sep=('][', ')(', '}{')):
+
+def readBsv(fileName=None, text=None, sep=('][', ')(', '}{')):
     response = {}
-    f = open(fileName, 'r')
-    unsplitBsv = f.read()
+    if(fileName is not None):
+        f = open(fileName, 'r')
+        unsplitBsv = f.read()
+    elif(text is not None):
+        unsplitBsv = text
+
     firstSep = unsplitBsv[0:2]
     splitBsv = deque(unsplitBsv.split(sep[0]))
 
     if (firstSep == sep[0]):
-        rows = splitBsv.count('')
-        columns = (len(splitBsv) - splitBsv.count('') - 1) / splitBsv.count('')
+        #print 'first'
         cleanList = filter(lambda a: a != '', splitBsv)
-        for c in range(columns):
+        rows = len(splitBsv) - len(cleanList) - 1
+        #print rows
+        columns = [i for i, a in enumerate(splitBsv) if a != '']
+        nbrOfColumns = len(columns) / rows
+        #print nbrOfColumns
+        cleanList = filter(lambda a: a != '', splitBsv)
+        #print cleanList
+        for c in range(nbrOfColumns):
             col = deque()
             for r in range(0, rows):
-                col.append(cleanList[(columns * r) + c])
+                col.append(cleanList[(nbrOfColumns * r) + c])
 
-            #response.append(col)
             colName = 'X' + str(c + 1)
             response[colName] = col
 
-
     if (firstSep == sep[1]):
-        rows = splitBsv.count(sep[1])
-        columns = (len(splitBsv) - splitBsv.count(sep[1]) - 1) / splitBsv.count(sep[1])
+        rows = splitBsv.count(sep[1]) - 1
+        columns = [i for i, a in enumerate(splitBsv) if a != sep[1]]
+        nbrOfColumns = len(columns) / rows
         cleanList = filter(lambda a: a != sep[1], splitBsv)
-        for c in range(columns):
+        for c in range(nbrOfColumns):
             col = deque()
             for r in range(0, rows):
-                col.append(cleanList[(columns * r) + c])
+                col.append(cleanList[(nbrOfColumns * r) + c])
 
-            #response.append(col)
             colName = 'X' + str(c + 1)
             response[colName] = col
 
     if (firstSep == sep[2]):
         response = {}
         hdr = sep[2] + sep[1]
-        splitBsv.remove(sep[2])
-        rows = splitBsv.count(sep[1])
-        colName = splitBsv.popleft()
-        while colName != hdr:
+        hdrTxt = splitBsv[0]
+        splitHdr = hdrTxt.split(sep[2])
+        splitHdr = splitHdr[1:len(splitHdr) - 1]
+        splitBsv = [splitBsv[i] for i in range(1, len(splitBsv))]
+        rows = 0
+        for val in splitBsv:
+            if val == sep[1]:
+                rows = rows + 1
+
+        for colName in splitHdr:
             response[colName] = deque()
-            colName = splitBsv.popleft()
 
         columns = len(response)
         cleanList = filter(lambda a: a != sep[1], splitBsv)
-        for c, colName in enumerate(response.keys()):
+        for c, colName in enumerate(splitHdr):
             col = deque()
-            for r in range(rows + 1):
+            for r in range(rows):
                 col.append(cleanList[(columns * r) + c])
 
             response[colName] = col
 
     return response
+
 
 def writeBsv(x, filepath=None, sep=('][', ')(', '}{')):
     response = ""
@@ -73,3 +88,8 @@ def writeBsv(x, filepath=None, sep=('][', ')(', '}{')):
         f = open(filepath, 'w')
         f.write(response)
         f.close()
+
+
+#print readBsv(text='][b][s][][v][o][][m][g][')
+#print readBsv(text=")(][b][s][v][)(][o][m][g][)(")
+#print readBsv(text="}{Col1}{Col2}{)(][b][s][)(][v][o][)(][m][g][)(")
